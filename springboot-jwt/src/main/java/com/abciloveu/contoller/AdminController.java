9396875a8ac367b727b9818d714a7d487a801fb4
Sorasak Chotsiri <sorasak_cho@truecorp.co.th>
2024-03-06 21:31:03 +0700

    [add] springboot jwt

---
+++ springboot-jwt/src/main/java/com/abciloveu/contoller/AdminController.java
@@ -0,0 +1,17 @@
+package com.abciloveu.contoller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class AdminController {
+
+    @GetMapping("/admin/configs")
+    public ResponseEntity<List<String>> configs() {
+        return ResponseEntity.ok(Arrays.asList("config1", "config2"));
+    }
+}
---
+++ springboot-jwt/src/test/java/com/abciloveu/AppJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abciloveu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
